9c310940799f497d15513a779241c289c9dbe39a
Manisha <manishap9766@gmail.com>
2026-02-05 22:08:58 +0530

    Using JUnit AllowanceCalculator

--- allowance/AllowanceCalculator.java
+++ allowance/AllowanceCalculator.java
@@ -1,27 +1,11 @@
-import java.util.Scanner;
-
 public class AllowanceCalculator {
 
-    public static double calculateAllowance(double basicSalary) {
+    public double calculateAllowance(double basicSalary) {
 
         double hra = 0.20 * basicSalary;
         double da  = 0.10 * basicSalary;
         double ta  = 0.05 * basicSalary;
 
         return hra + da + ta;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter Basic Salary: ");
-        double basicSalary = sc.nextDouble();
-
-        double totalAllowance = calculateAllowance(basicSalary);
-
-        System.out.println("Total Allowance: " + totalAllowance);
-
-        sc.close();
     }
 }
--- allowance/AllowanceCalculatorTest.java
+++ allowance/AllowanceCalculatorTest.java
@@ -6,19 +6,10 @@ public class AllowanceCalculatorTest {
     @Test
     public void testCalculateAllowance() {
 
-        // Test case 1
-        Assert.assertEquals(3500.0,
-                AllowanceCalculator.calculateAllowance(10000),
-                0.001);
+        AllowanceCalculator calc = new AllowanceCalculator();
 
-        // Test case 2
-        Assert.assertEquals(1750.0,
-                AllowanceCalculator.calculateAllowance(5000),
-                0.001);
-
-        // Test case 3
-        Assert.assertEquals(0.0,
-                AllowanceCalculator.calculateAllowance(0),
-                0.001);
+        Assert.assertEquals(3500.0, calc.calculateAllowance(10000), 0.001);
+        Assert.assertEquals(1750.0, calc.calculateAllowance(5000), 0.001);
+        Assert.assertEquals(0.0,    calc.calculateAllowance(0), 0.001);
     }
 }
